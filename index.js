18f7bc745d782ac9902eb730b60cfd91b14a7639
krimi <ny.kalash@gmail.com>
2022-02-27 14:55:21 +0100

    second commit - test graphql

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-import './src'
+import { graphql, buildSchema } from 'graphql'
+
+const db = {
+  users: [
+    { id: 1, name: 'Nico', email: 'ny.kalash@gmail.com' },
+    { id: 2, name: 'Krimi', email: 'krimi@gmail.com' }
+  ]
+}
+
+const schema = buildSchema(`
+    type Query {
+        users: [User!]!
+    }
+
+    type User {
+      id: ID!
+      name: String
+      email: String
+      avatarUrl: String
+    }
+`)
+
+const rootValue = {
+  users: () => db.users
+}
+
+graphql(
+  schema,
+  `
+    {
+      users {
+        email
+      }
+    }
+  `,
+  rootValue
+)
+  .then(({ data }) => console.log(data))
+  .catch(console.error)
--- src/resolvers/user.js
+++
@@ -1,9 +0,0 @@
-export default {
-  Query: {
-    users: (root, args, context, info) => {},
-    user: (root, args, context, info) => {}
-  },
-  Mutation: {
-    signup: (root, args, context, info) => {}
-  }
-}
