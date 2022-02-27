a5d42d6146ede27ddebad5a36ee9a4e6ddf1418a
krimi <ny.kalash@gmail.com>
2022-02-27 13:54:20 +0100

    first commit - test apollo server express

---
+++ src/resolvers/user.js
@@ -0,0 +1,9 @@
+export default {
+  Query: {
+    users: (root, args, context, info) => {},
+    user: (root, args, context, info) => {}
+  },
+  Mutation: {
+    signup: (root, args, context, info) => {}
+  }
+}
